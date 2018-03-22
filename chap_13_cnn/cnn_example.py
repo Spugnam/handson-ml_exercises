934b864df012c1c9ffe216b7d99a5be212b60743
Spugnam <quentin.picard@gmail.com>
2018-03-22 11:14:39 -0500

    add chap_13_cnn/cnn_example.py

---
+++ chap_13_cnn/cnn_example.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin//python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.datasets import load_sample_image
+
+# Load sample images
+china = load_sample_image("china.jpg")
+flower = load_sample_image("flower.jpg")
+dataset = np.array([china, flower], dtype=np.float32)
+batch_size, height, width, channels = dataset.shape
+
+# Create 2 filters
+filters = np.zeros(shape=(7, 7, channels, 2), dtype=np.float32)
+filters[:, 3, :, 0] = 1  # vertical line
+filters[3, :, :, 1] = 1  # horizontal line
+
+# Create a graph with input X plus a convolutional layer applying the 2 filters
+X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+# None: will be number of samples
+convolution = tf.nn.conv2d(X, filters, strides=[1,2,2,1], padding="SAME")
+
+with tf.Session() as sess:
+    output = sess.run(convolution, feed_dict={X: dataset})
+
+# vertical filter
+plt.imshow(output[0, :, :, 0], cmap="gray")
+
+# horizontal filter
+plt.imshow(output[0, :, :, 1], cmap="gray")  # plot 1st image's 2nd feature map
+plt.show()
